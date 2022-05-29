6c384db0969c16a54fff70e618e0c45c05367bb3
timmymike <t19880916@yahoo.com.tw>
2022-05-29 17:41:09 +0800

    修正為選擇日期區間

--- app/src/main/java/com/timmy/datepickerdemo/MainActivity.kt
+++ app/src/main/java/com/timmy/datepickerdemo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.datepicker.MaterialDatePicker
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -16,29 +17,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.btn_show).setOnClickListener {
-            DatePickerDialog(this@MainActivity).run {
-
-                datePicker.setOnDateChangedListener { datePicker, year, monthOfYear, dayOfMonth ->
-                    datePicker.minDate = 0L
-                    datePicker.minDate = "$year/${monthOfYear + 1}/$dayOfMonth".toDate("yyyy/M/dd")?.time ?: 0L
+            MaterialDatePicker.Builder.dateRangePicker().setTitleText("選一個區間").build().run {
+                addOnPositiveButtonClickListener {
+                    this@MainActivity.findViewById<TextView>(R.id.tv_result).text = "Selected Date is : \n${it.first.toDate()}\nto\n${it.second.toDate()}"
                 }
-
-                setOnDateSetListener { _, year, monthOfYear, dayOfMonth ->
-                    this@MainActivity.findViewById<TextView>(R.id.tv_result).text = "Selected Date is : " + "$year/${monthOfYear + 1}/$dayOfMonth"
-                }
-
-                show()
+                show(supportFragmentManager, "dateRangePicker")
             }
+
+//            DatePickerDialog(this@MainActivity).run {
+//
+//                datePicker.setOnDateChangedListener { datePicker, year, monthOfYear, dayOfMonth ->
+//                    datePicker.minDate = 0L
+//                    datePicker.minDate = "$year/${monthOfYear + 1}/$dayOfMonth".toDate("yyyy/M/dd")?.time ?: 0L
+//                }
+//
+//                setOnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+//                    this@MainActivity.findViewById<TextView>(R.id.tv_result).text = "Selected Date is : " + "$year/${monthOfYear + 1}/$dayOfMonth"
+//                }
+//
+//                show()
+//            }
 
         }
     }
 }
 
-fun String.toDate(format: String): Date? {
-    try {
-        return SimpleDateFormat(format, Locale.getDefault()).parse(this)
-    } catch (e: ParseException) {
-        e.printStackTrace()
-    }
-    return null
+//fun String.toDate(format: String): Date? {
+//    try {
+//        return SimpleDateFormat(format, Locale.getDefault()).parse(this)
+//    } catch (e: ParseException) {
+//        e.printStackTrace()
+//    }
+//    return null
+//}
+
+fun Long.toDate(): Date {
+    return Date(this)
 }
